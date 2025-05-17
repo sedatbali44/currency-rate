ad25277c914e4714c02a9052a8e667739341a38c
Sedat Bali <sedatbali44@gmail.com>
2025-05-17 22:57:51 +0300

    initial commit

---
+++ src/test/java/com/currency_rate/currency_rate/CurrencyRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currency_rate.currency_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
